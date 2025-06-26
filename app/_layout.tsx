2d1e1195b3c3af3a33b9157c16d7946b6134e59a
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-06-26 19:57:45 -0300

    Aplicando Interface Gráfica

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,51 @@
-import React from "react";
-import { Text, View } from "react-native";
-
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 export default function ExibirCalculo(){
+    
+    const router = useRouter();
+        const[nome, setNome] = useState();
+        const[altura, setAltura] = useState();
+        const[peso, setPeso] = useState();
+    
     return(
         <View>
-            <Text>Calculo</Text>
+            <Text style={styles.title}>Bem Vindo ao App</Text>
+             <Text style={styles.blue}>Digite o seu nome:</Text>
+             <TextInput style={styles.text} onChangeText={setNome} value={nome}/>
+             
+             <Text style={styles.blue}>Digite sua Altura:</Text>
+             <TextInput style={styles.text} onChangeText={setAltura} value={altura}/>
+             
+             <Text style={styles.blue}>Digite o seu Peso:</Text>
+             <TextInput style={styles.text} onChangeText={setPeso} value={peso}/>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 0,
+    },
+
+    blue: {
+        borderWidth: 1,
+        borderColor: "blue",
+        color: "black",
+        fontSize: 25,
+        textAlign: "center"
+    },
+
+    text: {
+        fontSize: 25,
+        textAlign: "center"
+    },
+
+    title:{
+        borderWidth: 1,
+        backgroundColor: "red",
+        color: "white",
+        textAlign: "center",
+        fontSize: 25
+    }
+})
--- components/index.tsx
+++ components/index.tsx
@@ -1,17 +1,3 @@
-import { useRouter } from "expo-router";
-import React, { useState } from "react";
-import { Text, TextInput, View } from "react-native";
-
+import React from "react";
 export default function Components(){
-    const router = useRouter();
-    const[nome, setNome] = useState();
-    return(
-        <View>
-            <Text>Digite o seu nome:</Text>
-          <TextInput placeholder="Digite o seu nome" onChangeText={setNome} value={nome}/>
-          <Text>Digite sua Altura:</Text>
-          <TextInput/>
-
-        </View>
-    );
 }
